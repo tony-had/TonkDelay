fea40fae90b07456b3315d477cfaad6edbbb058d
Tony Hadjiivanov <tony.hadjiivanov@gmail.com>
2019-10-07 19:54:59 +0300

    Define delay parameters

--- Source/DelayParameters.h
+++ Source/DelayParameters.h
@@ -9,3 +9,30 @@
 */
 
 #pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+enum TremoloFloatParametersEnum
+{
+	delayTimeParameter = 0,
+	delayFeedbackParameter,
+	delayMixParameter,
+	totalNumFloatParameters
+};
+
+struct DelayFloatParameter
+{
+	String parameterId;
+	String parameterName;
+	NormalisableRange<float> normalisableRange;
+	float defaultValue;
+	String parameterLabel;
+};
+
+static DelayFloatParameter delayFloatParameters[totalNumFloatParameters] =
+{
+	{"DelayTime", "Delay Time", NormalisableRange<float>(0.f, 20.f), 0.5f, "s"},
+	{"Feedback", "Feedback", NormalisableRange<float>(0.f, 1.f), 0.5f, ""},
+	{"Mix", "Mix", NormalisableRange<float>(0.f, 1.f), 0.5f, ""}
+};
+
