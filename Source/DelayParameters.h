85af241089122e279bab16f3f59bbb37bbe37386
Tony Hadjiivanov <tony.hadjiivanov@gmail.com>
2019-10-11 17:02:58 +0300

    Add a global circularBufferSize constant

--- Source/DelayParameters.h
+++ Source/DelayParameters.h
@@ -29,9 +29,13 @@ struct DelayFloatParameter
 	String parameterLabel;
 };
 
+constexpr float delayTimeMax = 2.f;
+// TODO: move circularBufferSize to a new file - e.g. audio helpers or somesuch
+constexpr int circularBufferSize = delayTimeMax * 192000 + 1; //arbitrarily 192 000 is the largest sample rate this plugin will suppport
+
 static DelayFloatParameter delayFloatParameters[totalNumFloatParameters] =
 {
-	{"DelayTime", "Delay Time", NormalisableRange<float>(0.f, 20.f), 0.5f, "s"},
+	{"DelayTime", "Delay Time", NormalisableRange<float>(0.f, delayTimeMax), 1.f, "s"},
 	{"Feedback", "Feedback", NormalisableRange<float>(0.f, 1.f), 0.5f, ""},
 	{"Mix", "Mix", NormalisableRange<float>(0.f, 1.f), 0.5f, ""}
 };
